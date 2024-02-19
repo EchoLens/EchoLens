4d13f70380174f74c06076d5a1f8afd07db95b87
liziyang <zl489@njit.edu>
2024-02-19 13:58:24 -0800

    Spring project initiated

---
+++ src/main/java/Group/EchoLens/EchoLensApplication.java
@@ -0,0 +1,13 @@
+package Group.EchoLens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoLensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchoLensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Group/EchoLens/EchoLensApplicationTests.java
@@ -0,0 +1,13 @@
+package Group.EchoLens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchoLensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
